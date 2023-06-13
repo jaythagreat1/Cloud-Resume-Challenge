dbfbf3bd552629bb25d889e8514cd59296071dcf
Jaythgreat1 <mrhorner819@gmail.com>
2023-06-13 17:04:44 -0400

    Updated

--- .github/workflows/_test.py
+++ .github/workflows/_test.py
@@ -1,9 +1,23 @@
 import boto3
 
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('cloudresume')
+
+def lambda_handler(event, context):
+    response = table.get_item(Key={'id': '1'})
+    if 'Item' in response:
+        views = response['Item'].get('views', 0)
+        views += 1
+        print(f"The website has been viewed {views} times.")
+        response = table.put_item(Item={'id': '1', 'views': views})
+    else:
+        views = 1
+        print(f"The website has been viewed {views} time.")
+
+    return views
+
 def smoke_test():
     try:
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('cloudresume')
         event = {}
         context = {}
         result = lambda_handler(event, context)
